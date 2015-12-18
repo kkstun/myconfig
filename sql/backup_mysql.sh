e42b2fbee3091ea118382e012de66ee2bc2c7dcd
kkstun <kkstun@qq.com>
2015-12-18 08:53:20 +0800

    修改

--- sql/backup_mysql.sh
+++ sql/backup_mysql.sh
@@ -1,2 +1,7 @@
 /opt/lampp/bin/mysqldump -uparcel -pparcel2015 parceldb > /home/parcel/sqlbackup/`date +%Y%m%d`.sql
 find /home/parcel/sqlbackup/ -mtime +10 -name '*.sql' -exec rm -rf {} \;
+
+
+#用crontab添加计划任务
+#crontab -e
+#0 4 * * * /home/parcel/backup_mysql.sh
